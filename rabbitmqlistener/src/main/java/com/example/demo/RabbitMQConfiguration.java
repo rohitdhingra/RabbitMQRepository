ef0b59819790220e1e7cfdcb8cb3485dbd9007c2
Rohit Dhingra <er.rohit.dhingra@gmail.com>
2021-05-11 17:31:56 +0530

    MQ Listener and different ways of queue creation

---
+++ rabbitmqlistener/src/main/java/com/example/demo/RabbitMQConfiguration.java
@@ -0,0 +1,41 @@
+package com.example.demo;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.MessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class RabbitMQConfiguration {
+
+	private static final String MY_QUEUE = "MyQueue";
+	
+	@Bean
+	public Queue myQueue()
+	{
+		return new Queue(MY_QUEUE,true);
+	}
+	
+	@Bean
+	public ConnectionFactory connectionFactory()
+	{
+		CachingConnectionFactory cachingConnectionFactory = new CachingConnectionFactory("localhost");
+		cachingConnectionFactory.setUsername("guest");
+		cachingConnectionFactory.setPassword("guest");
+		return cachingConnectionFactory;
+	}
+	@Bean
+	public MessageListenerContainer messageListenerContainer()
+	{
+		SimpleMessageListenerContainer simpleMessageListenerContainer = new SimpleMessageListenerContainer();
+		simpleMessageListenerContainer.setConnectionFactory(connectionFactory());
+		simpleMessageListenerContainer.setQueues(myQueue());
+		simpleMessageListenerContainer.setMessageListener(new RabbitMQMessageListener());
+		return simpleMessageListenerContainer;
+		
+	}
+}
